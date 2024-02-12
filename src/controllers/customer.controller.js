b6d26def3a517110b5fd84870b25c8667a6364c4
desarrollo4@historyclinical.com <desarrollo4@historyclinical.com>
2024-02-12 12:58:51 -0500

    se agrega modulo de actualizacion de clientes

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -37,7 +37,78 @@ export const getCustomerByCompany = async (req, res) => {
         return res.json({ status: 204, item });
       }
   } catch (error) {
-    console.log(error)
     res.status(500).json({ error: "Error al obtener las categorias" });
+  }
+};
+
+//Actualizar el estado de un cliente
+export const updateCustomerStatusById = async (req, res) => {
+  const _id = req.params._id;
+  const newStatus = req.body.status; // Asegúrate de enviar el nuevo estado en el cuerpo de la solicitud
+
+  try {
+    // Buscar el cliente por su ID y actualizar el estado
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      _id,
+      { status: newStatus },
+      { new: true } // Devuelve el documento actualizado
+    );
+
+    // Verificar si el cliente existe y fue actualizado
+    if (updatedCustomer) {
+      return res.json({
+        httpStatus: HTTP_OK,
+        message: "Estado del cliente actualizado con éxito",
+        status: "success",
+        updated: updatedCustomer,
+      });
+    } else {
+      // Si el cliente no existe
+      return res.status(HTTP_NOT_FOUND).json({
+        httpStatus: HTTP_NOT_FOUND,
+        message: "Cliente no encontrado",
+        status: "error",
+      });
+    }
+  } catch (error) {
+    res.status(HTTP_INTERNAL_SERVER_ERROR).json({ error: error.message });
+  }
+};
+
+// Actualizar un cliente por su ID
+export const updateCustomerById = async (req, res) => {
+  const _id = req.params._id;
+  // Extraer los campos que se pueden actualizar
+  const {
+    cedula, firstName, lastName, phone, address, comments, idCompany
+  } = req.body;
+
+  try {
+    // Buscar el Cliente por su ID y actualizar los campos proporcionados
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      _id,
+      {
+        cedula, firstName, lastName, phone, address, comments, idCompany
+      },
+      { new: true } // Devuelve el documento actualizado
+    );
+    // Verificar si el Cliente existe y fue actualizado
+    if (updatedCustomer) {
+      return res.json({
+        httpStatus: HTTP_OK,
+        message: "Cliente actualizado con éxito",
+        status: "success",
+        updated: updatedCustomer,
+      });
+    } else {
+      // Si el cliente no existe
+      return res.status(HTTP_NOT_FOUND).json({
+        httpStatus: HTTP_NOT_FOUND,
+        message: "Cliente no encontrado",
+        status: "error",
+      });
+    }
+  } catch (error) {
+    res.status(HTTP_INTERNAL_SERVER_ERROR).json({ error: error.message });
   }
 };
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
-  createCustomer, getCustomerByCompany
+  createCustomer, getCustomerByCompany, updateCustomerById, updateCustomerStatusById
 } from "../controllers/customer.controller.js";
-import { get } from "mongoose";
 
 const router = express.Router();
 
 //create Company
 router.post("/", createCustomer);
 router.get("/company/:idCompany", getCustomerByCompany);
+router.patch("/updateCustomer/:_id", updateCustomerById);
+router.put("/updateStatus/:_id", updateCustomerStatusById);
 
 export default router;
